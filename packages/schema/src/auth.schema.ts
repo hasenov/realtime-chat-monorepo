c0494d74427a77f5d6dc198eae392909abff2f67
Aidar <aidarwebdev@gmail.com>
2025-12-20 19:47:39 +0500

    feat(shared): add zod auth schemas

---
+++ packages/schema/src/auth.schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const RegisterSchema = z.object({
+    email: z.email(),
+    username: z.string().min(3),
+    password: z.string().min(6),
+});
+
+export const LoginSchema = RegisterSchema.pick({
+    email: true,
+    password: true,
+});
+
+export type RegisterInput = z.infer<typeof RegisterSchema>;
